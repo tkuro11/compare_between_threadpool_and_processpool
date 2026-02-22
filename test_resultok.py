34087219225da3d23a62f00e84630f9c3aa2bb6b
Takayuki Kurosawa <tkuro11@gmail.com>
2026-02-22 17:20:35 +0900

    test refined

--- test_resultok.py
+++ test_resultok.py
@@ -1,45 +1,43 @@
+from _pytest.capture import _windowsconsoleio_workaround
 import pytest
 
 from comparison import sumup, sumup_numpy, sequential, threadpool, processpool
 
 
-@pytest.fixture
-def div(request):
-    n, nthread = request.param
+def make_division(n: int, nthread: int) -> list[range]:
     div = n // nthread
-    division = [range(div * i + 1, div * i + div + 1) for i in range(nthread)]
-    return division
+    return [range(div * i + 1, div * i + div + 1) for i in range(nthread)]
 
 
 N = 1_000_000
 SUM_EXPECTED = (1 + N) * N // 2
 
 
-@pytest.mark.parametrize("div", [(N, 10), (N, 20)], indirect=True)
-def test_sequential_result(div):
-    assert sequential(div, 1, sumup) == SUM_EXPECTED
+@pytest.mark.parametrize("nthread", [10, 20])
+@pytest.mark.parametrize(
+    "runner, max_wks, worker",
+    [
+        (sequential, 1, sumup),
+        (threadpool, 10, sumup),
+        (sequential, 1, sumup_numpy),
+        (threadpool, 10, sumup_numpy),
+    ],
+)
+def test_result_fast(nthread, runner, max_wks, worker):
+    division = make_division(N, nthread)
+    assert runner(division, max_wks, sumup) == SUM_EXPECTED
 
 
-@pytest.mark.parametrize("div", [(N, 10), (N, 20)], indirect=True)
-def test_threadpool_result(div):
-    assert threadpool(div, 10, sumup) == SUM_EXPECTED
-
-
-@pytest.mark.parametrize("div", [(N, 10), (N, 20)], indirect=True)
-def test_processpool_result(div):
-    assert processpool(div, 10, sumup) == SUM_EXPECTED
-
-
-@pytest.mark.parametrize("div", [(N, 10), (N, 20)], indirect=True)
-def test_sequential_numpy_result(div):
-    assert sequential(div, 1, sumup_numpy) == SUM_EXPECTED
-
-
-@pytest.mark.parametrize("div", [(N, 10), (N, 20)], indirect=True)
-def test_threadpool_numpy_result(div):
-    assert threadpool(div, 10, sumup_numpy) == SUM_EXPECTED
-
-
-@pytest.mark.parametrize("div", [(N, 10), (N, 20)], indirect=True)
-def test_processpool_numpy_result(div):
-    assert processpool(div, 10, sumup_numpy) == SUM_EXPECTED
+@pytest.mark.slow
+@pytest.mark.unstable
+@pytest.mark.parametrize("nthread", [10, 20])
+@pytest.mark.parametrize(
+    "runner, max_wks, worker",
+    [
+        (processpool, 10, sumup),
+        (processpool, 10, sumup_numpy),
+    ],
+)
+def test_result_unstable(nthread, runner, max_wks, worker):
+    division = make_division(N, nthread)
+    assert runner(division, max_wks, worker) == SUM_EXPECTED

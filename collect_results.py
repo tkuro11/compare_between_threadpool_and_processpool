8e47d109e34be054a2236b4585b818f571ab123a
Takayuki Kurosawa <tkuro11@gmail.com>
2026-02-25 07:54:33 +0900

    fix AMD ryzen. for unknown reason, ryzen results are not correct in GIL on/off

--- collect_results.py
+++ collect_results.py
@@ -8,15 +8,20 @@ machine = cpuinfo.get_cpu_info()['brand_raw']
 python_version = cpuinfo.platform.python_version()
 
 subprocess.run(["uv","python","pin", "3.14"])
+
 filename = f"{dirname}/results/{machine}-python{python_version}-GIL.log"
+os.environ["PYTHON_GIL"]="1"
+subprocess.run(["uv","run","python", "-c","import sys; print('GIL:', sys._is_gil_enabled());"])
 print("collecting GIL benchmark results ...",end="",flush=True)
 result = subprocess.run(["uv", "run", "comparison.py"], capture_output=True)
 print("done")
 with open(filename, "w") as f:
     f.write(result.stdout.decode('ascii'))
 
-subprocess.run(["uv","python","pin", "3.14t"])
+
 filename = f"{dirname}/results/{machine}-python{python_version}-free_thread.log"
+os.environ["PYTHON_GIL"]="0"
+subprocess.run(["uv","run","python", "-c","import sys; print('GIL:', sys._is_gil_enabled());"])
 print("collecting thread-free benchmark results ...",end="",flush=True)
 result = subprocess.run(["uv", "run", "comparison.py"], capture_output=True)
 print("done")
